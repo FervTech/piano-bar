d62a6e931875d1e4a3566f72e06925a137cff9fb
Faisel-ali <161211192+Faisel-ali@users.noreply.github.com>
2026-07-15 23:47:13 +0000

    additional order structure

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      // Automatically polyfill all Node built‑ins
-      include: ['crypto', 'stream', 'util', 'buffer', 'process'],
-      globals: {
-        Buffer: true,
-        process: true,
-      },
-    }),
-  ],
-  // You no longer need to externalize Node built‑ins
-  // build: { rolldownOptions: { external: [] } }
+  plugins: [react()],
 })
